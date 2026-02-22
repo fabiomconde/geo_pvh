fc833166b07e67ba5e5ba2d8db33af96f6d21320
Fabio Conde <fabiomconde@gmail.com>
2026-02-22 10:16:35 -0400

    não reiniciar ngrok se em execução ao atualizar o docker

--- django_app/core_gis/apps.py
+++ django_app/core_gis/apps.py
@@ -19,19 +19,26 @@ class CoreGisConfig(AppConfig):
                 ngrok.set_auth_token("6o1VwvnAHhM4dD2LNW6xU_4TTCH95bMqXBy3QY6SnSq")
                 options = {"auth": "plataforma:pvh2026!", "bind_tls": True}
                 
-                # Desconecta túneis anteriores pra evitar limite de conta grátis
-                ngrok.kill()
-                
-                # Inicia túnel na porta configurada, default 8000
-                port = sys.argv[sys.argv.index('runserver') + 1] if len(sys.argv) > sys.argv.index('runserver') + 1 else "8000"
-                if port.startswith('0.0.0.0:'):
-                    port = port.split(':')[1]
+                active_tunnels = ngrok.get_tunnels()
+                if active_tunnels:
+                    public_url = active_tunnels[0].public_url
+                    print(f"==================================================")
+                    print(f"⚡ NGROK TUNNEL ALREADY ACTIVE: {public_url}")
+                    print(f"==================================================")
+                else:
+                    # Desconecta túneis anteriores pra evitar limite de conta grátis
+                    ngrok.kill()
+                    
+                    # Inicia túnel na porta configurada, default 8000
+                    port = sys.argv[sys.argv.index('runserver') + 1] if len(sys.argv) > sys.argv.index('runserver') + 1 else "8000"
+                    if port.startswith('0.0.0.0:'):
+                        port = port.split(':')[1]
 
-                public_url = ngrok.connect(port, **options).public_url
-                print(f"==================================================")
-                print(f"🚀 NGROK TUNNEL INITIALIZED: {public_url}")
-                print(f"🔒 Credentials -> User: plataforma / Pass: pvh2026!")
-                print(f"==================================================")
+                    public_url = ngrok.connect(port, **options).public_url
+                    print(f"==================================================")
+                    print(f"🚀 NGROK TUNNEL INITIALIZED: {public_url}")
+                    print(f"🔒 Credentials -> User: plataforma / Pass: pvh2026!")
+                    print(f"==================================================")
                 
                 # Injeta a variável env pro Django considerar durante o requests a APIs internas via URL absoluta, se houver
                 os.environ["NGROK_PUBLIC_URL"] = public_url
